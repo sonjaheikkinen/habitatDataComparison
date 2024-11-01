7822d650ae8ec46e7da7a966eca7b4dc32fce651
Sonja Heikkinen <sonja.t.heikkinen@outlook.com>
2024-11-01 12:30:51 +0200

    Design structure for the main script

---
+++ main.R
@@ -0,0 +1,46 @@
+# THIS SCRIPT WAS MADE FOR THESIS PROJECT:
+# COMPARING EU HABITAT TYPES AND CORINE LAND COVER AS PREDICTORS FOR BIRD DISTRIBUTION IN NORTHERN LAPLAND
+# SONJA HEIKKINEN, 2024, LIFE SCIENCE INFORMATICS
+
+# STUDY QUESTIONS:
+# 1. How accurately can bird distributions and diversity be predicted using habitat types?
+# 2. How does the accuracy compare to other data (such as Corine)? 
+# 3. Does the accuracy differ between species? # If the accuracy (or difference in accuracy between habitat/corine) 
+# differs between species, what can explain these differences?
+
+# SETTINGS FOR RUNNING THE SCRIPT
+
+# DIRECTORY PATHS
+
+# ALL NECESSARY LIBRARIES
+
+# GLOBALLY USED FUNCTIONS
+
+# SCRIPT STARTS
+#################################################################################################################
+
+# PREPROCESS DATA
+
+# READ IN DATA
+
+# FORMAT DATA
+
+# EXPLORATORY ANALYSIS
+
+# SELECT DATA
+
+# DEFINE MODELS
+
+# FIT MODELS
+
+# CHECK MODEL CONVERGENCE
+
+# CHECK MODEL FITS
+
+# COMPARE MODEL FITS
+
+# EXPLORE REASONS FOR DIFFERENCES
+
+
+
+
