1a0e72c878eb526764adf8be3134afce9cabaea7
Sonja Heikkinen <sonja.t.heikkinen@outlook.com>
2024-11-01 13:02:18 +0200

    Limit transects to area that has natura habitat type data

--- preprocess_data.R
+++ preprocess_data.R
@@ -4,6 +4,20 @@
 
 
 # FUNCTIONS FOR PREPROCESSING
+get_transects_that_overlap_raster <- function(transects, raster) {
+    raster_values_for_transects <- extract(raster, transects)
+    transects_with_no_NA_values <- c()
+    for (transect_id in unique(raster_values_for_transects$ID)) {
+        values_for_transect <- raster_values_for_transects[raster_values_for_transects$ID == transect_id,]$natura_2393
+        if (sum(is.na(values_for_transect)) < 0.1 * length(values_for_transect)) {
+            transects_with_no_NA_values <- c(transects_with_no_NA_values, transect_id)
+        }
+    }
+    filtered_transects <- subset(transects,
+                                 transects$ID %in% transects_with_no_NA_values)
+    
+    return(filtered_transects)
+}
 
 # SCRIPT STARTS
 ###################################################################################################################
@@ -13,6 +27,8 @@ transects_shp <- vect(file.path(dir_data, "bird_transects.shp"))
 observations <- read.csv(file.path(dir_data, "birdtransects_toRecbase.csv"))
 
 # LIMIT DATA TO STUDY AREA
+transects_shp$ID <- 1:nrow(transects_shp) # Adding numerical id, because existing chr identifier caused issues
+transects_that_overlap_natura_raster <- get_transects_that_overlap_raster(transects_shp, natura_raster)
 
 # CLEAN DATA
 
