cf944156fe0e5a5eefe3a6f3600b3fb71fa2a95b
Sonja Heikkinen <sonja.t.heikkinen@outlook.com>
2024-11-01 17:44:40 +0200

    Write down information on raw data

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -13,14 +13,19 @@ file <- file.path(dir_results, "exploratory_analysis.txt")
 
 # LOAD RAW DATA
 load(file = file.path(dir_data, "occurrence_raw.RData")) # occurrence
+load(file = file.path(dir_data, "natura_raster.RData")) # natura_raster
+load(file = file.path(dir_data, "fractions_natura.RData")) # fractions_natura
 
 # BASIC NUMBERS ON RAW DATA
 # Occurrence
 number_of_species <- length(colnames(occurrence))
 append_to_file(sprintf("Number of species: %s\n", number_of_species), file)
-# Natura raster
-number_of_habitats_natura <- length(unique(habitat_types_raster)[,1])
+# Natura habitats
+number_of_habitats_natura <- length(unique(natura_raster)[,1])
 append_to_file(sprintf("Number of natura habitats: %s\n", number_of_habitats_natura), file)
+number_of_known_habitats_within_buffer_natura <- length(colnames(fractions_natura))
+append_to_file(sprintf("Number of known natura habitats within buffer: %s\n", 
+                       number_of_known_habitats_within_buffer_natura), file)
 
 
 
