ab99b22c64e299b7023e33e48f73016a8df2892f
Sonja Heikkinen <sonja.t.heikkinen@outlook.com>
2024-11-21 17:55:59 +0200

    Fix typo from filename into which the fitted models are written

--- fit_models.R
+++ fit_models.R
@@ -17,7 +17,7 @@ for (i in 1:length(model_list)) {
     for (j in 1:length(thinning_values)) {
         
         thinning_value <- thinning_values[j]
-        model_file <- file.path(dir_fitted, sprintf("%s_thin_%s_fitted.Rdata", 
+        model_file <- file.path(dir_fitted, sprintf("%s_thin_%s_fitted.RData", 
                                                         model_name,
                                                         thinning_value))
         found <- file.exists(model_file)
@@ -34,7 +34,7 @@ for (i in 1:length(model_list)) {
                                        thin = thinning_value, 
                                        transient = round(0.2 * number_of_samples * thinning_value),
                                        nChains = 4, 
-                                       #nParallel = 4, #If parallel is set, verbose prints do not show
+                                       nParallel = 4, #If parallel is set, verbose prints do not show
                                        verbose = 10)
             
             print(sprintf("Thinning value: %s, ended %s", thinning_value, date()))
