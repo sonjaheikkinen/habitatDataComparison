abf7869f53ce4a72733083f1e49f5619c4673147
Sonja Heikkinen <sonja.t.heikkinen@outlook.com>
2025-01-23 17:22:47 +0200

    Organice convergence plots by thinning value

--- check_model_convergence.R
+++ check_model_convergence.R
@@ -23,11 +23,20 @@ save_convergence_info <- function(samples, variable, model_name, convergence_fil
     plot_convergence(variable, model_name, psrf_point_estimates)
 }
 
+extract_thinning_value <- function(filename) {
+    parts <- strsplit(filename, "_")[[1]] 
+    thinning_value <- as.numeric(parts[length(parts) - 1])
+    print(thinning_value)
+    return(thinning_value)
+}
+
 
 # SCRIPT STARTS
 
 # List filenames of fitted models
 fitted_models <- list.files(dir_fitted, pattern="*.RData", full.names=TRUE)
+model_thinning_values <- sapply(fitted_models, extract_thinning_value)
+fitted_models <- fitted_models[order(model_thinning_values)]
 
 # Create file for convergence plots
 pdf(file = file.path(dir_results,"convergence_results.pdf"))
@@ -45,9 +54,12 @@ for (model_number in 1:length(fitted_models)) {
     thinning_value <- strsplit(model_name, "_")[[1]][4]
     number_of_random_levels <- model$nr
     
+    print(sprintf("Calculating convergence for %s", model_name))
+    
     # GET THE POSTERIOR SAMPLES OF PARAMETERS AS CODA OBJECT
     # Get only names (not numbers) of species and covariates
     posterior <- convertToCodaObject(model, spNamesNumbers = c(T,F), covNamesNumbers = c(T,F))
+    print("Converted to coda object")
     
     # CALCULATE THE CONVERGENCE FOR EACH VARIABLE
     # First get posterior samples for each variable
@@ -62,17 +74,20 @@ for (model_number in 1:length(fitted_models)) {
     append_to_file("BETA\n\n", file = convergence_file)
     samples_beta <- posterior$Beta # 
     save_convergence_info(samples_beta, "beta", model_name, convergence_file)
+    print("Calculated convergence for beta")
     
     # CONVERGENCE FOR INFLUENCE OF TRAITS (GAMMA)
     append_to_file("GAMMA\n\n", file = convergence_file)
     samples_gamma <- posterior$Gamma
     save_convergence_info(samples_gamma, "gamma", model_name, convergence_file)
+    print("Calculated convergence for gamma")
     
     # CONVERGENCE FOR INFLUENCE OF PHYLOGENY (RHO)
     if (!is.null(posterior$Rho)) {
         append_to_file("RHO\n\n", file = convergence_file)
         samples_rho <- posterior$Rho
         save_convergence_info(samples_rho, "rho", model_name, convergence_file)
+        print("Calculated convergence for rho")
     }
     
     if (number_of_random_levels > 0) {
@@ -95,6 +110,7 @@ for (model_number in 1:length(fitted_models)) {
                                   sprintf("omega, %s", randomlevel_name), 
                                   model_name, 
                                   convergence_file)
+            print(sprintf("Calculated convergence for omega %s", randomlevel_name))
         }
         
         
@@ -112,6 +128,7 @@ for (model_number in 1:length(fitted_models)) {
                                       sprintf("alpha, %s", randomlevel_name), 
                                       model_name, 
                                       convergence_file)
+                print(sprintf("Calculated convergence for alpha %s", randomlevel_name))
             } else {
                 append_to_file(sprintf("%s is not a spatial random level", randomlevel_name), convergence_file)
             }
