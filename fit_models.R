0540856f41ae7102ca2b463d3ac1b74ad8bbf990
Sonja Heikkinen <sonja.t.heikkinen@outlook.com>
2025-05-09 14:15:57 +0300

    Fit models using thinning

--- fit_models.R
+++ fit_models.R
@@ -30,8 +30,8 @@ for (i in 1:length(model_list)) {
             print(sprintf("Thinning value: %s, started %s", thinning_value, date()))
             
             fitted_model <- sampleMcmc(model_to_fit, 
-                                       samples = number_of_samples * thinning_value, 
-                                       thin = 1,
+                                       samples = number_of_samples, 
+                                       thin = thinning_value,
                                        transient = ceiling(0.5 * number_of_samples * thinning_value),
                                        nChains = 4,
                                        nParallel = 4, 
@@ -39,7 +39,7 @@ for (i in 1:length(model_list)) {
             
             print(sprintf("Thinning value: %s, ended %s", thinning_value, date()))
             
-            #save(fitted_model, file = model_file)
+            save(fitted_model, file = model_file)
         }
     }
     
