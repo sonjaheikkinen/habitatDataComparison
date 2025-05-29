e267f3444210d461a0b9e3c7e315506c06af0408
Sonja Heikkinen <sonja.t.heikkinen@outlook.com>
2025-05-29 14:34:56 +0300

    Improve convergence plotting

--- check_model_convergence.R
+++ check_model_convergence.R
@@ -19,21 +19,31 @@ plot_convergence <- function(variable, model_name, psrf_point_estimates) {
     vioplot(psrf_point_estimates,
             col = "dodgerblue",
             ylim = c(0.9,1.1))
-    title(sprintf("PSRF (%s): %s", variable, model_name), 
+    title(sprintf("PSRF (%s):\n %s\n[%s, %s], mean %s, median %s, sd %s", 
+                  variable, 
+                  model_name,
+                  round(min(psrf_point_estimates), 3), 
+                  round(max(psrf_point_estimates), 3),
+                  round(mean(psrf_point_estimates), 3), 
+                  round(median(psrf_point_estimates), 3),
+                  round(sd(psrf_point_estimates), 3)), 
           outer = TRUE,
           line = -3)
     par(mfrow=c(1,1))
 } 
 
 save_convergence_info <- function(samples, variable, model_name, convergence_file) {
-    samples <- filter_zero_columns(samples)
+    #samples <- filter_zero_columns(samples)
     psrf <- gelman.diag(samples, multivariate = FALSE)$psrf
     append_to_file(sprintf("%s\n", summary(psrf)[,1]), file = convergence_file)
     append_to_file("\n\n", file = convergence_file)
     psrf_point_estimates <- psrf[,1]
     plot_convergence(variable, model_name, psrf_point_estimates)
+    plot_histogram(effectiveSize(samples),
+                   sprintf("Effective size of %s", variable))
     combined_samples <- as.mcmc.list(samples)
-    traceplot(combined_samples[,1])
+    traceplot(combined_samples[,1],
+              main = sprintf("Traceplot of %s", variable))
     
 }
 
@@ -44,6 +54,7 @@ filter_zero_columns <- function(samples) {
     filtered_samples <- lapply(samples, function(chain) chain[, !columns_zero_in_any_chain, drop = FALSE])
     return(filtered_samples)  
 }
+
 
 
 # SCRIPT STARTS
@@ -87,25 +98,83 @@ for (model_number in 1:length(fitted_models)) {
     
     # CONVERGENCE FOR SPECIES NICHES (BETA)
     append_to_file("BETA\n\n", file = convergence_file)
-    samples_beta <- posterior$Beta 
-    save_convergence_info(samples_beta, "beta", model_name, convergence_file)
+    samples_beta <- posterior$Beta
+    save_convergence_info(samples_beta, "Beta - species niches", model_name, convergence_file)
     print("Calculated convergence for beta")
     
     # CONVERGENCE FOR INFLUENCE OF TRAITS (GAMMA)
     append_to_file("GAMMA\n\n", file = convergence_file)
     samples_gamma <- posterior$Gamma
-    save_convergence_info(samples_gamma, "gamma", model_name, convergence_file)
+    save_convergence_info(samples_gamma, "Gamma - influence of traits", model_name, convergence_file)
     print("Calculated convergence for gamma")
+    
+    # CONVERGENCE FOR UNEXPLAINED VARIATION IN SPECIES NICHES (V)
+    append_to_file("V\n\n", file = convergence_file)
+    samples_v <- posterior$V
+    save_convergence_info(samples_v, "V - unexplained variation in species niches", model_name, convergence_file)
+    print("Calculated convergence for V")
+    
+    # CONVERGENCE FOR RESIDUAL VARIANCE (SIGMA)
+    #append_to_file("SIGMA\n\n", file = convergence_file)
+    #samples_sigma <- posterior$Sigma
+    #save_convergence_info(samples_sigma, "Sigma - residual variance", model_name, convergence_file)
+    #print("Calculated convergence for Sigma")
+    
     
     # CONVERGENCE FOR INFLUENCE OF PHYLOGENY (RHO)
     if (!is.null(posterior$Rho)) {
         append_to_file("RHO\n\n", file = convergence_file)
         samples_rho <- posterior$Rho
-        save_convergence_info(samples_rho, "rho", model_name, convergence_file)
+        save_convergence_info(samples_rho, "Rho - influence of phylogeny", model_name, convergence_file)
         print("Calculated convergence for rho")
     }
     
     if (number_of_random_levels > 0) {
+        
+        
+        # CONVERGENCE FOR SITE LOADINGS (ETA)
+        append_to_file("ETA\n\n", file = convergence_file)
+        samples_eta <- posterior$Eta
+        for (randomlevel_number in 1:number_of_random_levels) {
+            randomlevel_name <- names(model$ranLevels)[randomlevel_number]
+            append_to_file(sprintf("%s\n\n", randomlevel_name), file = convergence_file)
+            samples_eta_randomlevel <- samples_eta[[randomlevel_number]]
+            randomlevel_length <- dim(samples_eta_randomlevel[[1]])[2]
+            if (randomlevel_length > 1000) {
+                random_sample_indices <- sample(1:randomlevel_length, size = 1000)
+                for (chain in 1:length(samples_eta_randomlevel)) {
+                    samples_eta_randomlevel[[chain]] <- samples_eta_randomlevel[[chain]][,random_sample_indices]
+                }
+            }
+            save_convergence_info(samples_eta_randomlevel, 
+                                  sprintf("Eta - site loadings, %s", randomlevel_name), 
+                                  model_name, 
+                                  convergence_file)
+            print(sprintf("Calculated convergence for Eta %s", randomlevel_name))
+        }
+        
+        
+        # CONVERGENCE FOR SPECIES LOADINGS (LAMBDA)
+        append_to_file("LAMBDA\n\n", file = convergence_file)
+        samples_lambda <- posterior$Lambda
+        for (randomlevel_number in 1:number_of_random_levels) {
+            randomlevel_name <- names(model$ranLevels)[randomlevel_number]
+            append_to_file(sprintf("%s\n\n", randomlevel_name), file = convergence_file)
+            samples_lambda_randomlevel <- samples_lambda[[randomlevel_number]]
+            randomlevel_length <- dim(samples_lambda_randomlevel[[1]])[2]
+            if (randomlevel_length > 1000) {
+                random_sample_indices <- sample(1:randomlevel_length, size = 1000)
+                for (chain in 1:length(samples_lambda_randomlevel)) {
+                    samples_lambda_randomlevel[[chain]] <- samples_lambda_randomlevel[[chain]][,random_sample_indices]
+                }
+            }
+            save_convergence_info(samples_lambda_randomlevel, 
+                                  sprintf("Lambda - species loadings, %s", randomlevel_name), 
+                                  model_name, 
+                                  convergence_file)
+            print(sprintf("Calculated convergence for Lambda %s", randomlevel_name))
+        }
+        
         
         # CONVERGENCE FOR RANDOM VARIATION IN CO-OCCURRENCE (OMEGA)
         append_to_file("OMEGA\n\n", file = convergence_file)
@@ -122,7 +191,7 @@ for (model_number in 1:length(fitted_models)) {
                 }
             }
             save_convergence_info(samples_omega_randomlevel, 
-                                  sprintf("omega, %s", randomlevel_name), 
+                                  sprintf("Omega - random variation in co-occurrence, %s", randomlevel_name), 
                                   model_name, 
                                   convergence_file)
             print(sprintf("Calculated convergence for omega %s", randomlevel_name))
@@ -140,7 +209,7 @@ for (model_number in 1:length(fitted_models)) {
             if (number_of_spatial_dimensions > 0) {
                 samples_alpha_randomlevel <- samples_alpha[[randomlevel_number]] 
                 save_convergence_info(samples_alpha_randomlevel, 
-                                      sprintf("alpha, %s", randomlevel_name), 
+                                      sprintf("Alpha - spatial scale, %s", randomlevel_name), 
                                       model_name, 
                                       convergence_file)
                 print(sprintf("Calculated convergence for alpha %s", randomlevel_name))
@@ -148,6 +217,51 @@ for (model_number in 1:length(fitted_models)) {
                 append_to_file(sprintf("%s is not a spatial random level", randomlevel_name), convergence_file)
             }
         }
+        
+        
+        # CONVERGENCE FOR LOCAL SHRINKAGE OF SPECIES LOADINGS (PSI)
+        append_to_file("LAMBDA\n\n", file = convergence_file)
+        samples_psi <- posterior$Psi
+        for (randomlevel_number in 1:number_of_random_levels) {
+            randomlevel_name <- names(model$ranLevels)[randomlevel_number]
+            append_to_file(sprintf("%s\n\n", randomlevel_name), file = convergence_file)
+            samples_psi_randomlevel <- samples_psi[[randomlevel_number]]
+            randomlevel_length <- dim(samples_psi_randomlevel[[1]])[2]
+            if (randomlevel_length > 1000) {
+                random_sample_indices <- sample(1:randomlevel_length, size = 1000)
+                for (chain in 1:length(samples_psi_randomlevel)) {
+                    samples_psi_randomlevel[[chain]] <- samples_psi_randomlevel[[chain]][,random_sample_indices]
+                }
+            }
+            save_convergence_info(samples_psi_randomlevel, 
+                                  sprintf("Psi - local shrinkage of species loadings (?), %s", randomlevel_name), 
+                                  model_name, 
+                                  convergence_file)
+            print(sprintf("Calculated convergence for Psi %s", randomlevel_name))
+        }
+        
+        
+        # CONVERGENCE FOR GLOBAL SHRINKAGE OF SPECIES LOADINGS (DELTA)
+        append_to_file("DELTA\n\n", file = convergence_file)
+        samples_delta <- posterior$Delta
+        for (randomlevel_number in 1:number_of_random_levels) {
+            randomlevel_name <- names(model$ranLevels)[randomlevel_number]
+            append_to_file(sprintf("%s\n\n", randomlevel_name), file = convergence_file)
+            samples_delta_randomlevel <- samples_delta[[randomlevel_number]]
+            randomlevel_length <- dim(samples_delta_randomlevel[[1]])[2]
+            if (randomlevel_length > 1000) {
+                random_sample_indices <- sample(1:randomlevel_length, size = 1000)
+                for (chain in 1:length(samples_delta_randomlevel)) {
+                    samples_delta_randomlevel[[chain]] <- samples_delta_randomlevel[[chain]][,random_sample_indices]
+                }
+            }
+            save_convergence_info(samples_delta_randomlevel, 
+                                  sprintf("Delta - global shrinkage of species loadings, %s", randomlevel_name), 
+                                  model_name, 
+                                  convergence_file)
+            print(sprintf("Calculated convergence for Delta %s", randomlevel_name))
+        }
+        
         
         
     }
